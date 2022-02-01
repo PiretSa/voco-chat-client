4002587897ceff0dc5a4e1473527ff07bba699c2
PiretSa <piret.saar.003@khk.ee>
2022-02-01 20:16:24 +0200

    Added React bootstrap and minor styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Chat from "./components/Chat";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h3>VOCO</h3>
+          <div>
+              <Chat/>
+          </div>
       </header>
     </div>
   );
---
+++ src/components/Chat.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../App.css';
+
+function Chat() {
+    return (
+        <div className="Chat">
+            <div className="box">
+
+
+            </div>
+        </div>
+
+    );
+}
+export default Chat;
